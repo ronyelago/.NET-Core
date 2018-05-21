5e609565fa62bb94112c2d91d70b27ba079bc5dc
Ronye do Lago <ronye.rocha@gmail.com>
2018-05-21 14:38:12 -0300

    subindo tudo novamente

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hosting = new WebHostBuilder()
+                .UseKestrel()
+                .Configure(
+                    app => {
+                        app.Run(context => context.Response.WriteAsync("oi meu chapa"));
+                    }
+                )
+                .Build();
+
+            hosting.Run();
+        }
+    }
+}
